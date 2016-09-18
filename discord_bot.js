885433e9a040b09187af5ca0efa479f33ff22ee9
koad <koad@kingofalldata.com>
2016-09-18 18:09:06 -0400

    some_message

---
+++ discord_bot.js
@@ -0,0 +1,418 @@
+try {
+	var Discord = require("discord.js");
+} catch (e){
+	console.log(e.stack);
+	console.log(process.version);
+	console.log("Please run npm install and ensure it passes with no errors!");
+	process.exit();
+}
+
+try {
+	var yt = require("./youtube_plugin");
+	var youtube_plugin = new yt();
+} catch(e){
+	console.log("couldn't load youtube plugin!\n"+e.stack);
+}
+
+// try {
+// 	var wa = require("./wolfram_plugin");
+// 	var wolfram_plugin = new wa();
+// } catch(e){
+// 	console.log("couldn't load wolfram plugin!\n"+e.stack);
+// }
+
+// Get authentication data
+try {
+	var AuthDetails = require("./auth.json");
+} catch (e){
+	console.log("Please create an auth.json like auth.json.example with a bot token or an email and password.\n"+e.stack);
+	process.exit();
+}
+
+// Load custom permissions
+var Permissions = {};
+try{
+	Permissions = require("./permissions.json");
+} catch(e){}
+Permissions.checkPermission = function (user,permission){
+	try {
+		var allowed = false;
+		try{
+			if(Permissions.global.hasOwnProperty(permission)){
+				allowed = Permissions.global[permission] == true;
+			}
+		} catch(e){}
+		try{
+			if(Permissions.users[user.id].hasOwnProperty(permission)){
+				allowed = Permissions.users[user.id][permission] == true;
+			}
+		} catch(e){}
+		return allowed;
+	} catch(e){}
+	return false;
+}
+
+//load config data
+var Config = {};
+try{
+	Config = require("./config.json");
+} catch(e){ //no config file, use defaults
+	Config.debug = false;
+	Config.respondToInvalid = false;
+}
+
+var qs = require("querystring");
+
+var d20 = require("d20");
+
+var htmlToText = require('html-to-text');
+
+var startTime = Date.now();
+
+var giphy_config = {
+    "api_key": "dc6zaTOxFJmzC",
+    "rating": "r",
+    "url": "http://api.giphy.com/v1/gifs/random",
+    "permission": ["NORMAL"]
+};
+
+
+//https://api.imgflip.com/popular_meme_ids
+var meme = {
+	"brace": 61546,
+	"mostinteresting": 61532,
+	"fry": 61520,
+	"onedoesnot": 61579,
+	"yuno": 61527,
+	"success": 61544,
+	"allthethings": 61533,
+	"doge": 8072285,
+	"drevil": 40945639,
+	"skeptical": 101711,
+	"notime": 442575,
+	"yodawg": 101716
+};
+
+var aliases;
+var messagebox;
+
+var commands = {
+    "version": {
+        description: "returns the git commit this bot is running",
+        process: function(bot,msg,suffix) {
+            var commit = require('child_process').spawn('git', ['log','-n','1']);
+            commit.stdout.on('data', function(data) {
+                bot.sendMessage(msg.channel,data);
+            });
+            commit.on('close',function(code) {
+                if( code != 0){
+                    bot.sendMessage(msg.channel,"failed checking git version!");
+                }
+            });
+        }
+    },
+    "log": {
+        usage: "<log message>",
+        description: "logs message to bot console",
+        process: function(bot,msg,suffix){console.log(msg.content);}
+    },
+    "rss": {
+        description: "lists available rss feeds",
+        process: function(bot,msg,suffix) {
+            /*var args = suffix.split(" ");
+            var count = args.shift();
+            var url = args.join(" ");
+            rssfeed(bot,msg,url,count,full);*/
+            bot.sendMessage(msg.channel,"Available feeds:", function(){
+                for(var c in rssFeeds){
+                    bot.sendMessage(msg.channel,c + ": " + rssFeeds[c].url);
+                }
+            });
+        }
+    },
+	"eval": {
+		usage: "<command>",
+		description: 'Executes arbitrary javascript in the bot process. User must have "eval" permission',
+		process: function(bot,msg,suffix) {
+			if(Permissions.checkPermission(msg.author,"eval")){
+				bot.sendMessage(msg.channel, eval(suffix,bot));
+			} else {
+				bot.sendMessage(msg.channel, msg.author + " doesn't have permission to execute eval!");
+			}
+		}
+	},
+	"topic": {
+		usage: "[topic]",
+		description: 'Sets the topic for the channel. No topic removes the topic.',
+		process: function(bot,msg,suffix) {
+			bot.setChannelTopic(msg.channel,suffix);
+		}
+	},
+    "uptime": {
+    	usage: "",
+	description: "returns the amount of time since the bot started",
+	process: function(bot,msg,suffix){
+		var now = Date.now();
+		var msec = now - startTime;
+		console.log("Uptime is " + msec + " milliseconds");
+		var days = Math.floor(msec / 1000 / 60 / 60 / 24);
+		msec -= days * 1000 * 60 * 60 * 24;
+		var hours = Math.floor(msec / 1000 / 60 / 60);
+		msec -= hours * 1000 * 60 * 60;
+		var mins = Math.floor(msec / 1000 / 60);
+		msec -= mins * 1000 * 60;
+		var secs = Math.floor(msec / 1000);
+		var timestr = "";
+		if(days > 0) {
+			timestr += days + " days ";
+		}
+		if(hours > 0) {
+			timestr += hours + " hours ";
+		}
+		if(mins > 0) {
+			timestr += mins + " minutes ";
+		}
+		if(secs > 0) {
+			timestr += secs + " seconds ";
+		}
+		bot.sendMessage(msg.channel,"Uptime: " + timestr);
+	}
+    }
+};
+try{
+var rssFeeds = require("./rss.json");
+function loadFeeds(){
+    for(var cmd in rssFeeds){
+        commands[cmd] = {
+            usage: "[count]",
+            description: rssFeeds[cmd].description,
+            url: rssFeeds[cmd].url,
+            process: function(bot,msg,suffix){
+                var count = 1;
+                if(suffix != null && suffix != "" && !isNaN(suffix)){
+                    count = suffix;
+                }
+                rssfeed(bot,msg,this.url,count,false);
+            }
+        };
+    }
+}
+} catch(e) {
+    console.log("Couldn't load rss.json. See rss.json.example if you want rss feed commands. error: " + e);
+}
+
+try{
+	aliases = require("./alias.json");
+} catch(e) {
+	//No aliases defined
+	aliases = {};
+}
+
+try{
+	messagebox = require("./messagebox.json");
+} catch(e) {
+	//no stored messages
+	messagebox = {};
+}
+function updateMessagebox(){
+	require("fs").writeFile("./messagebox.json",JSON.stringify(messagebox,null,2), null);
+}
+
+function rssfeed(bot,msg,url,count,full){
+    var FeedParser = require('feedparser');
+    var feedparser = new FeedParser();
+    var request = require('request');
+    request(url).pipe(feedparser);
+    feedparser.on('error', function(error){
+        bot.sendMessage(msg.channel,"failed reading feed: " + error);
+    });
+    var shown = 0;
+    feedparser.on('readable',function() {
+        var stream = this;
+        shown += 1
+        if(shown > count){
+            return;
+        }
+        var item = stream.read();
+        bot.sendMessage(msg.channel,item.title + " - " + item.link, function() {
+            if(full === true){
+                var text = htmlToText.fromString(item.description,{
+                    wordwrap:false,
+                    ignoreHref:true
+                });
+                bot.sendMessage(msg.channel,text);
+            }
+        });
+        stream.alreadyRead = true;
+    });
+}
+
+
+var bot = new Discord.Client();
+
+bot.on("ready", function () {
+    // loadFeeds();
+	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+	require("./plugins.js").init();
+});
+
+bot.on("disconnected", function () {
+
+	console.log("Disconnected!");
+	process.exit(1); //exit node.js with an error
+	
+});
+
+bot.on("message", function (msg) {
+	//check if message is a command
+	if(msg.author.id != bot.user.id && (msg.content[0] === '!' || msg.content.indexOf(bot.user.mention()) == 0)){
+        console.log("treating " + msg.content + " from " + msg.author + " as command");
+		var cmdTxt = msg.content.split(" ")[0].substring(1);
+        var suffix = msg.content.substring(cmdTxt.length+2);//add one for the ! and one for the space
+        if(msg.content.indexOf(bot.user.mention()) == 0){
+			try {
+				cmdTxt = msg.content.split(" ")[1];
+				suffix = msg.content.substring(bot.user.mention().length+cmdTxt.length+2);
+			} catch(e){ //no command
+				bot.sendMessage(msg.channel,"Yes?");
+				return;
+			}
+        }
+		alias = aliases[cmdTxt];
+		if(alias){
+			console.log(cmdTxt + " is an alias, constructed command is " + alias.join(" ") + " " + suffix);
+			cmdTxt = alias[0];
+			suffix = alias[1] + " " + suffix;
+		}
+		var cmd = commands[cmdTxt];
+
+
+
+
+        if(cmdTxt === "help"){
+            //help is special since it iterates over the other commands
+            var helpMessage = "Available Commands:"
+            var i=0;
+            for(var cmd in commands) {
+                i++
+                helpMessage += "\n\t!" + cmd;
+                var usage = commands[cmd].usage;
+                if(usage){
+                    helpMessage += " " + usage;
+                };
+                var description = commands[cmd].description;
+                if(description){
+                    helpMessage += " - " + description;
+                };
+                if(i>10){
+                    bot.sendMessage(msg.author,helpMessage);
+                    i=0;
+                    helpMessage = "";
+                };
+            };
+            bot.sendMessage(msg.author,helpMessage);
+        }
+
+
+
+
+
+		else if(cmd) {
+			try{
+				cmd.process(bot,msg,suffix);
+			} catch(e){
+				if(Config.debug){
+					bot.sendMessage(msg.channel, "command " + cmdTxt + " failed :(\n" + e.stack);
+				}
+			}
+		} else {
+			if(Config.respondToInvalid){
+				bot.sendMessage(msg.channel, "Invalid command " + cmdTxt);
+			}
+		}
+	} else {
+		//message isn't a command or is from us
+        //drop our own messages to prevent feedback loops
+        if(msg.author == bot.user){
+            return;
+        }
+        
+        if (msg.author != bot.user && msg.isMentioned(bot.user)) {
+                bot.sendMessage(msg.channel,msg.author + ", you called?");
+        }
+    }
+});
+ 
+
+//Log user status changes
+bot.on("presence", function(user,status,gameId) {
+	//if(status === "online"){
+	//console.log("presence update");
+	console.log(user+" went "+status);
+	//}
+	try{
+	if(status != 'offline'){
+		if(messagebox.hasOwnProperty(user.id)){
+			console.log("found message for " + user.id);
+			var message = messagebox[user.id];
+			var channel = bot.channels.get("id",message.channel);
+			delete messagebox[user.id];
+			updateMessagebox();
+			bot.sendMessage(channel,message.content);
+		}
+	}
+	}catch(e){}
+});
+
+function get_gif(tags, func) {
+        //limit=1 will only return 1 gif
+        var params = {
+            "api_key": giphy_config.api_key,
+            "rating": giphy_config.rating,
+            "format": "json",
+            "limit": 1
+        };
+        var query = qs.stringify(params);
+
+        if (tags !== null) {
+            query += "&tag=" + tags.join('+')
+        }
+
+        //wouldnt see request lib if defined at the top for some reason:\
+        var request = require("request");
+        //console.log(query)
+        request(giphy_config.url + "?" + query, function (error, response, body) {
+            //console.log(arguments)
+            if (error || response.statusCode !== 200) {
+                console.error("giphy: Got error: " + body);
+                console.log(error);
+                //console.log(response)
+            }
+            else {
+                try{
+                    var responseObj = JSON.parse(body)
+                    func(responseObj.data.id);
+                }
+                catch(err){
+                    func(undefined);
+                }
+            }
+        }.bind(this));
+    }
+exports.addCommand = function(commandName, commandObject){
+    try {
+        commands[commandName] = commandObject;
+    } catch(err){
+        console.log(err);
+    }
+}
+exports.commandCount = function(){
+    return Object.keys(commands).length;
+}
+if(AuthDetails.bot_token){
+	console.log("logging in with token");
+	bot.loginWithToken(AuthDetails.bot_token);
+} else {
+	console.log("Logging in as a user account. Consider switching to an official bot account instead!");
+	bot.login(AuthDetails.email, AuthDetails.password);
+}
